23a1a41b7214ec52bd3724cdac0900bbc14e018b
kvsaisriharsha-dev <kvsaisriharsha2005@gmail.com>
2026-02-15 14:12:41 +0530

    Update Memory.js

--- models/Memory.js
+++ models/Memory.js
@@ -2,21 +2,13 @@ import mongoose from "mongoose";
 
 const memorySchema = new mongoose.Schema(
   {
-    title: {
-      type: String,
-      required: true
-    },
-    date: {
-      type: Date,
-      required: true
-    },
-    note: {
-      type: String,
-      required: true
-    },
-    imageUrl: {
-      type: String
-    }
+    title: { type: String, required: true },
+    note: { type: String, required: true },
+    date: { type: Date, required: true },
+    imageUrl: { type: String },
+
+    aiCaption: { type: String },
+    mood: { type: String }
   },
   { timestamps: true }
 );
